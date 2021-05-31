caa21c57a6bb01248dc8b31e28c5724ba90421b3
Mauricio <Mauricio@DESKTOP-H31JITJ>
2021-05-31 15:20:05 -0500

    Versión 1 del proyecto

---
+++ tallerpracticogithub/src/tallerpracticogithub/Tallerpracticogithub.java
@@ -0,0 +1,11 @@
+package Tallerpracticogithub;
+public class Tallerpracticogithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola ");
+    }
+    
+}
