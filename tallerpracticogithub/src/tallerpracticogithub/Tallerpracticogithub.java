b51111ba01022e4edb6790d9b48c675b3d14f227
maurojasc <33296864+maurojasc@users.noreply.github.com>
2021-05-31 15:26:16 -0500

    Update Tallerpracticogithub.java

--- tallerpracticogithub/src/tallerpracticogithub/Tallerpracticogithub.java
+++ tallerpracticogithub/src/tallerpracticogithub/Tallerpracticogithub.java
@@ -6,6 +6,7 @@ public class Tallerpracticogithub {
      */
     public static void main(String[] args) {
         System.out.println("Hola ");
+        system.out.println("version 2");
     }
     
 }
