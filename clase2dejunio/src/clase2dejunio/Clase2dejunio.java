d14d054aee768228267d6261e193d8a6f1337252
Mauricio <Mauricio@DESKTOP-H31JITJ>
2021-06-02 11:51:35 -0500

    commit 1 versión 1 rama wmrc

--- clase2dejunio/src/clase2dejunio/Clase2dejunio.java
+++ clase2dejunio/src/clase2dejunio/Clase2dejunio.java
@@ -16,6 +16,7 @@ public class Clase2dejunio {
      */
     public static void main(String[] args) {
         System.out.println("versión 0");
+        System.out.println("Versión 1 rama wmrc");
     }
     
 }
